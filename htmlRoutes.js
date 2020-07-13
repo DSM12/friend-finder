4a970a813e84981e99275b93bb699033b492feee
Danny Montano <dsmontano12@gmail.com>
2020-07-13 18:42:17 -0400

    working through functionality

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,5 +1,7 @@
 // Should include 2 routes: A GET Route to "/survey" which should display the survey page.
-
+app.get("/survey", function(req, res) {
+    res.render("sur", { sur: survey });
+  });
 
 
 // & A default, catch-all route that leads to "home.html" which displays the home page.
--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-// Set up required npm packages
+// Set up required npm packages & path:
+
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require(path.join(__dirname, "./api"))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 
+// LISTENER
+// The below code effectively "starts" our server
 
-// Path:
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
