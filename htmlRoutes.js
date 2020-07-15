4153eae4ad24e45f238ac47c89edfa9872759bcf
Danny Montano <dsmontano12@gmail.com>
2020-07-15 01:42:13 -0400

    updated htmlRoutes file

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,7 +1,15 @@
 // Should include 2 routes: A GET Route to "/survey" which should display the survey page.
-app.get("/survey", function(req, res) {
-    res.render("sur", { sur: survey });
+var path = require("path");
+
+module.exports = function (app) {
+
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, "app\public\survey.html"));
   });
 
+  // & A default, catch-all route that leads to "home.html" which displays the home page.
+  app.use("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "app\public\home.html"));
+  });
 
-// & A default, catch-all route that leads to "home.html" which displays the home page.
+}; 
