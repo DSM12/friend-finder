9c31af2b15024cb2406fbcfdae767fe85cc43b47
Danny Montano <dsmontano12@gmail.com>
2020-07-14 20:38:11 -0400

    server.js updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Set up required npm packages & path:
 
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 // Sets up the Express App
@@ -10,6 +11,12 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + ""));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "" }));
 
 // ROUTER
 // The below points our server to a series of "route" files.
