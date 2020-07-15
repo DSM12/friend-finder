b8258876152a7bd9d3ec1bb30d1b25ecd40edc22
Danny Montano <dsmontano12@gmail.com>
2020-07-15 02:13:13 -0400

    friends array was added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,31 @@
 //APP data should be saved as an array of objects. Each object should roughly follow the format below:
+var friends = [
+    {
+        name: "Shawn",
+        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.xxlmag.com%2Fjay-z-shawn-carter-foundation-gala-6-million-dollars%2F&psig=AOvVaw01j4S6y0EzkOhC7oV4t6O5&ust=1594879721049000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODg5YzMzuoCFQAAAAAdAAAAABAD",
+        scores: [3, 2, 5, 5, 5, 2, 2, 5, 3, 4]
+    },
+    {
+        name: "Warren",
+        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fwarren-buffett%2F&psig=AOvVaw3JzPUg8lZ9A0Zjews7VIuF&ust=1594879759263000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDXkJ_MzuoCFQAAAAAdAAAAABAD",
+        scores: [2, 3, 4, 4, 3, 5, 2, 4, 4, 1]
+    },
+    {
+        name: "Bill",
+        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ABill_Gates_mugshot.png&psig=AOvVaw2ghnPWSU9KlBgsyKUYPqq9&ust=1594879803278000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOizibTMzuoCFQAAAAAdAAAAABAD",
+        scores: [5, 4, 5, 4, 4, 4, 5, 4, 5, 4]
+    },
+    {
+        name: "Michelle",
+        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftime.com%2F5768073%2Fmichelle-obama-playlist-workout-2020%2F&psig=AOvVaw3W0lc633mn8_zuqY5gc-Nu&ust=1594879837616000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLD2u8PMzuoCFQAAAAAdAAAAABAE",
+        scores: [1, 1, 2, 2, 2, 1, 2, 2, 5, 5]
+    },
+    {
+        name: "Pablo",
+        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fenglish.elpais.com%2Felpais%2F2013%2F10%2F17%2Finenglish%2F1382032171_533607.html&psig=AOvVaw3b59qUOFVUwFtXn2hj1Hvh&ust=1594879873130000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjZ-uHMzuoCFQAAAAAdAAAAABAD",
+        scores: [5, 5, 1, 2, 5, 1, 5, 4, 5, 5]
+    }
 
+];
 
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   
+module.exports = friends;
